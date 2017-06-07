71165a5c39e694d2d96eb146094de8c59c22f350
Your Name <your@example.com>
2017-06-07 12:26:51 +0300

    first commit

---
+++ src/Controllers/NotificationsController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Kordy\Ticketit\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Kordy\Ticketit\Helpers\LaravelVersion;
+use Kordy\Ticketit\Models\Comment;
+use Kordy\Ticketit\Models\Setting;
+use Kordy\Ticketit\Models\Ticket;
+
+class NotificationsController extends Controller
+{
+    public function newComment(Comment $comment)
+    {
+        $ticket = $comment->ticket;
+        $notification_owner = $comment->user;
+        $template = 'ticketit::emails.comment';
+        $data = ['comment' => serialize($comment), 'ticket' => serialize($ticket)];
+
+        $this->sendNotification($template, $data, $ticket, $notification_owner,
+            trans('ticketit::lang.notify-new-comment-from').$notification_owner->name.trans('ticketit::lang.notify-on').$ticket->subject, 'comment');
+    }
+
+    public function ticketStatusUpdated(Ticket $ticket, Ticket $original_ticket)
+    {
+        $notification_owner = auth()->user();
+        $template = 'ticketit::emails.status';
+        $data = [
+            'ticket'             => serialize($ticket),
+            'notification_owner' => serialize($notification_owner),
+            'original_ticket'    => serialize($original_ticket),
+        ];
+
+        if (strtotime($ticket->completed_at)) {
+            $this->sendNotification($template, $data, $ticket, $notification_owner,
+                $notification_owner->name.trans('ticketit::lang.notify-updated').$ticket->subject.trans('ticketit::lang.notify-status-to-complete'), 'status');
+        } else {
+            $this->sendNotification($template, $data, $ticket, $notification_owner,
+                $notification_owner->name.trans('ticketit::lang.notify-updated').$ticket->subject.trans('ticketit::lang.notify-status-to').$ticket->status->name, 'status');
+        }
+    }
+
+    public function ticketAgentUpdated(Ticket $ticket, Ticket $original_ticket)
+    {
+        $notification_owner = auth()->user();
+        $template = 'ticketit::emails.transfer';
+        $data = [
+            'ticket'             => serialize($ticket),
+            'notification_owner' => serialize($notification_owner),
+            'original_ticket'    => serialize($original_ticket),
+        ];
+
+        $this->sendNotification($template, $data, $ticket, $notification_owner,
+            $notification_owner->name.trans('ticketit::lang.notify-transferred').$ticket->subject.trans('ticketit::lang.notify-to-you'), 'agent');
+    }
+
+    public function newTicketNotifyAgent(Ticket $ticket)
+    {
+        $notification_owner = auth()->user();
+        $template = 'ticketit::emails.assigned';
+        $data = [
+            'ticket'             => serialize($ticket),
+            'notification_owner' => serialize($notification_owner),
+        ];
+
+        $this->sendNotification($template, $data, $ticket, $notification_owner,
+            $notification_owner->name.trans('ticketit::lang.notify-created-ticket').$ticket->subject, 'agent');
+    }
+
+    /**
+     * Send email notifications from the action owner to other involved users.
+     *
+     * @param string $template
+     * @param array  $data
+     * @param object $ticket
+     * @param object $notification_owner
+     */
+    public function sendNotification($template, $data, $ticket, $notification_owner, $subject, $type)
+    {
+        /**
+         * @var User
+         */
+        $to = null;
+
+        if ($type != 'agent') {
+            $to = $ticket->user;
+
+            if ($ticket->user->email != $notification_owner->email) {
+                $to = $ticket->user;
+            }
+
+            if ($ticket->agent->email != $notification_owner->email) {
+                $to = $ticket->agent;
+            }
+        } else {
+            $to = $ticket->agent;
+        }
+
+        if (LaravelVersion::lt('5.4')) {
+            $mail_callback = function ($m) use ($to, $notification_owner, $subject) {
+                $m->to($to->email, $to->name);
+
+                $m->replyTo($notification_owner->email, $notification_owner->name);
+
+                $m->subject($subject);
+            };
+
+            if (Setting::grab('queue_emails') == 'yes') {
+                Mail::queue($template, $data, $mail_callback);
+            } else {
+                Mail::send($template, $data, $mail_callback);
+            }
+        } elseif (LaravelVersion::min('5.4')) {
+            $mail = new \Kordy\Ticketit\Mail\TicketitNotification($template, $data, $notification_owner, $subject);
+
+            if (Setting::grab('queue_emails') == 'yes') {
+                Mail::to($to)->queue($mail);
+            } else {
+                Mail::to($to)->send($mail);
+            }
+        }
+    }
+}
---
+++ src/Helpers/Cdn.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kordy\Ticketit\Helpers;
+
+/**
+ * This file containes the version of different external JS and CSS libraries
+ * that are included.
+ *
+ * Class Cdn
+ */
+class Cdn
+{
+    const CodeMirror = '5.26.0';
+    const Summernote = '0.8.3';
+    const FontAwesome = '4.7.0';
+}
